462156082839b6937a0a9accdff04a58d33db414
lydia <lydia@enroutejewelry.com>
2025-09-08 10:22:29 +0800

    fix: compile error

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 // import { generateClient } from "aws-amplify/data";
 
 // const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [todos, setTodos] = useState<Array<string>>([]);
 
   // useEffect(() => {
   //   client.models.Todo.observeQuery().subscribe({
@@ -13,17 +13,17 @@ function App() {
   //   });
   // }, []);
 
-  // function createTodo() {
-  //   client.models.Todo.create({ content: window.prompt("Todo content") });
-  // }
+  function createTodo() {
+    setTodos([...todos,""])
+  }
 
   return (
     <main>
       <h1>My todos</h1>
-      <button>+ new</button>
+      <button onClick={createTodo}>+ new</button>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+        {todos.map((todo,index) => (
+          <li key={todo+index}>{todo}</li>
         ))}
       </ul>
       <div>
